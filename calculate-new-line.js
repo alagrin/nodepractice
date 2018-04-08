76a7e886dcb9409edec932b036da6507d6685fe7
Alan R. Grinberg <grinny@icloud.com>
2018-04-07 20:52:50 -0700

    Update calculate-new-line.js
    updated comments

--- calculate-new-line.js
+++ calculate-new-line.js
@@ -1,12 +1,13 @@
 var fs = require('fs');
 //grab file and its contents- stored as object 
+
 var textObject = fs.readFileSync(process.argv[2]);
 //looking at the contents of the object- string
 
 //turn string into array of characters
 var stringArray = textObject.toString().split('');
-//take array and loop through contents
 
+//take array and loop through contents
 var count = 0; // declare in scope for loop to access
 
 //iterate through check if character is newline;
@@ -16,3 +17,5 @@ for (var i = 0; i < stringArray.length; i++) {
     } 
 }
 console.log(count);
+
+//test file is contents submitted in process object containing an argument with text
